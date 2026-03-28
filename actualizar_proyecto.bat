e5002a50600860b3f5617a7a2ef3d40c9d2ad93f
JAComunicacion <julio38arg@gmail.com>
2026-03-28 10:14:44 -0300

    Prueba de despliegue automático

---
+++ actualizar_proyecto.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "E:\CLIENTES\CLIENTES Concepcion\Karina Color&Estilo\TODO pLa WEB KARINA\KPS WEB"
+git add .
+git commit -m "Actualización automática de archivos"
+git push origin main
+pause
